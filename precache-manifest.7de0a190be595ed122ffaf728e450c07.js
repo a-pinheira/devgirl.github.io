f06d65bab97f2c1b6fae270eabb58feac1854946
monymachado <monymachad@yahoo.com.br>
2020-04-09 21:55:28 -0300

    Updates

---
+++ precache-manifest.7de0a190be595ed122ffaf728e450c07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a525d18b3b4e2e4f9c16661fe2a98550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f9067f3168903be0d31",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4aaf9a1a31c2bd19bde0",
+    "url": "/static/js/2.f31baaa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f31baaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9067f3168903be0d31",
+    "url": "/static/js/main.e2934526.chunk.js"
+  },
+  {
+    "revision": "a9ef7ab4efa2c60f720d",
+    "url": "/static/js/runtime-main.83299805.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
